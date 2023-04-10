2f3be0de857fea81cb4d4c3f048d01965668288b
developerkimtakgu <developerkimtakgu@gmail.com>
2023-04-10 09:53:39 +0900

    수정사항 - 복구

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $a = 1;
-    $b = 2;
-    $c = 3;
-
-    $d = ($a + $b) - 1;
-
-    echo $d . "<br>";
-
-
-    echo env('APP_NAME') . "<br>";
-    echo config('app.url') . "<br>";
-
-    echo "라라벨 테스트.. git에 제대로 올라갔는지.. pull 하시는분은 삭제바랍니다.";
     return view('welcome');
 });
