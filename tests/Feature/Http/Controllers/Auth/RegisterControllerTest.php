f6725bdd2a4c581cb18f696eb4f99817a4877914
leetaehee <lastride25@naver.com>
2023-04-23 22:24:02 +0900

    수정사항 -  회원가입 테스트

---
+++ tests/Feature/Http/Controllers/Auth/RegisterControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Event;
+use Tests\TestCase;
+
+class RegisterControllerTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    public function testReturnRegisterView()
+    {
+        $this->get(route('register'))
+            ->assertOk()
+            ->assertViewIs('auth.register');
+    }
+
+    public function testUserRegistration()
+    {
+        Event::fake();
+
+        $email = $this->faker->safeEmail;
+
+        $this->post(route('register'), [
+           'name' => $this->faker->name,
+           'email' => $email,
+           'password' => 'password',
+        ])->assertRedirect(
+            route('verification.notice')
+        );
+
+        $this->assertDatabaseHas('users', [
+           'email' => $email,
+        ]);
+
+        $this->assertAuthenticated();
+
+        Event::assertDispatched(Registered::class);
+    }
+}
