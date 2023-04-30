1e59d38a6ba73e1d364236300263f35211e9dcf1
leetaehee <lastride25@naver.com>
2023-04-30 18:53:31 +0900

    수정사항 - 회원가입 테스트

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -22,7 +22,8 @@ class RegisterController extends Controller
     public function register(RegisterUserRequest $request)
     {
         $user = User::create([
-            ...$request->validated(),
+            'name' => $request->name,
+            'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
