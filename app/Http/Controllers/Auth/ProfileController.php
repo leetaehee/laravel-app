9b6e01d254a6ee5399cad2ba875c54a683a5ce87
leetaehee <lastride25@naver.com>
2023-05-01 22:41:31 +0900

    수정사항 - 마이페이지 추가

---
+++ app/Http/Controllers/Auth/ProfileController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateProfileRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class ProfileController extends Controller
+{
+    /**
+     * Show the form for creating the resource.
+     */
+    public function create(): never
+    {
+        abort(404);
+    }
+
+    /**
+     * Store the newly created resource in storage.
+     */
+    public function store(Request $request): never
+    {
+        abort(404);
+    }
+
+    /**
+     * Display the resource.
+     */
+    public function show(Request $request)
+    {
+        $user = $request->user();
+
+        return view('auth.profile.show', [
+           'user' => $user
+        ]);
+    }
+
+    /**
+     * Show the form for editing the resource.
+     */
+    public function edit(Request $request)
+    {
+        $user = $request->user();
+
+        return view('auth.profile.edit', [
+           'user' => $user,
+        ]);
+    }
+
+    /**
+     * Update the resource in storage.
+     */
+    public function update(UpdateProfileRequest $request)
+    {
+        $user = $request->user();
+
+        $data = $request->validated();
+
+        if ($request->filled('password')) {
+            $data = [
+                ...$data,
+                'password' => Hash::make($request->password),
+            ];
+        }
+
+        $user->update($data);
+
+        return to_route('profile.show');
+    }
+
+    /**
+     * Remove the resource from storage.
+     */
+    public function destroy(): never
+    {
+        abort(404);
+    }
+}
